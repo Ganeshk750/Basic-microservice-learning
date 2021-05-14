b532eec7e0616f8ed3e61d8821b726d69719ecd8
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-05-14 22:11:25 +0530

    adding rating service in MovieCatalog service

--- movie-catalog-service/src/main/java/com/ganesh/controller/CatalogController.java
+++ movie-catalog-service/src/main/java/com/ganesh/controller/CatalogController.java
@@ -1,13 +1,16 @@
 package com.ganesh.controller;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ganesh.model.CatalogItem;
+import com.ganesh.model.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,6 +18,14 @@ public class CatalogController {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		return Collections.singletonList(new CatalogItem("Kick", "Action", 5));
+		//return Collections.singletonList(new CatalogItem("Kick", "Action", 5));
+		
+		
+		List<Rating> ratingsList = Arrays.asList(new Rating("1234", 4),
+				                  new Rating("5678", 5));
+		
+		return ratingsList.stream().map(rating -> 
+			    new CatalogItem("Kick", "Action", 5))
+				.collect(Collectors.toList());
 	}
 }
