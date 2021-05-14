11b4d39bd8203103cd6cce709eb25424464668f5
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-05-14 22:25:11 +0530

    ## adding Movie model in MovieCatalog Service & Using RestTemplete For Calling other Service

--- movie-catalog-service/src/main/java/com/ganesh/controller/CatalogController.java
+++ movie-catalog-service/src/main/java/com/ganesh/controller/CatalogController.java
@@ -8,8 +8,10 @@ import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.ganesh.model.CatalogItem;
+import com.ganesh.model.Movie;
 import com.ganesh.model.Rating;
 
 @RestController
@@ -21,11 +23,16 @@ public class CatalogController {
 		//return Collections.singletonList(new CatalogItem("Kick", "Action", 5));
 		
 		
-		List<Rating> ratingsList = Arrays.asList(new Rating("1234", 4),
-				                  new Rating("5678", 5));
+	 List<Rating> ratingsList = Arrays.asList(new Rating("1234", 4), new Rating("5678", 5));
 		
-		return ratingsList.stream().map(rating -> 
-			    new CatalogItem("Kick", "Action", 5))
-				.collect(Collectors.toList());
+	///	return ratingsList.stream().map(rating -> new CatalogItem("Kick", "Action", 5)).collect(Collectors.toList());
+	
+	    RestTemplate restTemplate = new RestTemplate();
+		return ratingsList.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Description", rating.getRating());
+        })
+        .collect(Collectors.toList());
+		
 	}
 }
